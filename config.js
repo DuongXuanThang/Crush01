0fb80ce66dd3558e0042ca5e6f7f86f921e788af
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-05-21 13:49:33 +0700

    second
    second

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Thảo à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,11 +9,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người và mình có cơ hội làm quen nhau hi vọng sắp tới chúng ta sẽ có những kỉ niệm đẹp',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Yêu anh',
+    mess: 'Anh biết mà 🥰😘😘',
+    messDesc: 'Hi vọng em sẽ vui khi đọc được đến dòng này',
+    btnAccept: 'Have a nice day',
+    messLink: 'https://www.instagram.com/dxt_97/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
